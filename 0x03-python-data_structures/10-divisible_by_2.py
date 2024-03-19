0f2f51d53c55662c3401989b5a435a389e0b100b
derhieakpos <derhieakpos@gmail.com>
2024-03-19 05:01:14 +0100

    Python data structure

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    is_divisible = []
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            is_divisible.append(i) = True
+        else:
+            is_divisible.append(i) = False
+        return (my_list)
