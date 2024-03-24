4f2f6c174d47488558407fbee8facf3595b4e89b
derhieakpos <derhieakpos@gmail.com>
2024-03-24 16:30:37 +0100

    Print Last digit and Combo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 lastDigit = abs(number) % 10
 if lastDigit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, lastDigit)) 
+    print("Last digit of {} is {} and is greater than 5".format(number, lastDigit))
 elif lastDigit == 0:
     print("Last digit of {} is {} and is 0".format(number, lastDigit))
 elif number < 6:
