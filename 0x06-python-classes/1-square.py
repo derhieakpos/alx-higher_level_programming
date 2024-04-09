3f5f534ae8cb2803db967570f6277df63ab40686
derhieakpos <derhieakpos@gmail.com>
2024-04-09 17:02:09 +0100

    A square without attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This model represents a square
+"""
+
+
+class Square:
+
+    """
+    this class represents a square
+
+    Attributes:
+        there are no attributes in this class
+
+    Methods:
+        there are no methods in this class
+    """
+    def __init__(self, size):
+        """
+        Initialization of a class instance
+
+        Args:
+            size: the size of the square
+        """
+
+        self.__size = size
