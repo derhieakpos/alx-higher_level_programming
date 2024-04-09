02cf39f6e9a381ceb707728cca300ffc08077e54
derhieakpos <derhieakpos@gmail.com>
2024-04-09 16:44:17 +0100

    Added comments to the file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""
+This model represents a sqare
+"""
+
+
 class Square:
+    """
+
+    this class represents a square
+
+    Attributes:
+        there are no attributes in this class
+
+    Methods:
+        there are no methods in this class
+
+    """
     pass
